90a7a5ee5fba98efbcd784906da8659282998372
Erwan Lajouannique <er.lajouannique@gmail.com>
2021-02-19 16:21:32 +0100

    .

--- is_builtin.c
+++ is_builtin.c
@@ -4,8 +4,8 @@ int     is_builtin(char *str)
 {
     if (!(ft_strcmp(str, "pwd")))
         return (1);
-    //if (!(ft_strcmp(str, "echo")))
-    //    return (1);
+    if (!(ft_strcmp(str, "echo")))
+        return (1);
     if (!(ft_strcmp(str, "env")))
         return (1);
     if (!(ft_strcmp(str, "cd")))
