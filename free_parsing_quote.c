d302abdea130e137748c88b9f769b25d6ef3674c
Erwan Lajouannique <er.lajouannique@gmail.com>
2021-02-20 17:32:44 +0100

    from ps

--- free_parsing_quote.c
+++
@@ -1,9 +0,0 @@
-#include "minishell.h"
-
-
-int			free_quote(t_cmd *cmd, char *prompt, int index)
-{
-	free_char_double_array(cmd->cmds);
-	free(prompt);
-	return (index);
-}
--- parsing_pipe.c
+++
@@ -1,8 +0,0 @@
-#include "minishell.h"
-
-int     parsing_pipe(t_cmd *cmd)
-{
-    cmd->pipe.nb_pipe = count_string(cmd->cmds, "|");
-    cmd->pipe.save = cmd->pipe.nb_pipe;
-    return (1);
-}
