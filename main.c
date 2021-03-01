96cc2286731820f51af9343f34de7474000a97eb
Erwan Lajouannique <er.lajouannique@gmail.com>
2021-03-01 09:58:32 +0100

    finish

--- main.c
+++ main.c
@@ -79,7 +79,7 @@ int		main(int i, char **av, char **envp)
 	i = 0;
 	if (!(envs = init_list_env(envp)))
 		return (-1);
-	//signal_handle();
+	signal_handle();
 	display_prompt();
 	while ((ret = get_next_line(0, &prompt, 0)) > 0)
 	{
