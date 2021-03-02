acfb2713467ee72b9bfcc1ae2df9249b16960e62
Votre Nom <thomassarafian@gmail.com>
2021-03-02 08:00:03 +0100

    modification prototype check_quote

--- minishell.h
+++ minishell.h
@@ -83,7 +83,8 @@ int     	get_next_line(int fd, char **line, int option);
 int     	ft_strlen(char *str);
 int     	parsing_line(char *prompt, t_list **envs, t_cmd *cmd);
 char    	**new_split(char *str, char c);
-// void		check_quote(char **str, char c, int *in_quote, char *tmp)
+void		check_quote(char **str, char c, int *in_quote, char *tmp);
+char		*my_strdup(char *str, int size);
 char    	*create_space_around(char *str);
 int	 		ft_strcmp(char *s1, char *s2);
 int     	free_parsing_line(char **av, int status);
