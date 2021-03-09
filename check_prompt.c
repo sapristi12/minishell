daa0232e423502e8fc71a8eaecc71fb25a2cc60f
Erwan Lajouannique <er.lajouannique@gmail.com>
2021-03-09 20:20:40 +0100

    llebi heehe

--- check_prompt.c
+++ check_prompt.c
@@ -6,30 +6,77 @@
 /*   By: erlajoua <erlajoua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 20:07:14 by erlajoua          #+#    #+#             */
-/*   Updated: 2021/03/01 20:18:04 by erlajoua         ###   ########.fr       */
+/*   Updated: 2021/03/09 20:18:25 by erlajoua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*int		check_semicolon(char *str)
+{
+	if (str[i] == ';' && str[i + 1] == ';')
+		return (0);
+	if (str[i] == ';')
+	{
+		i++;
+		while (str[i] && str[i] == ' ')
+			i++;
+		if (str[i] == ';')
+			return (0);
+	}
+}*/
+
+static int		is_first_condition(char tmp, char now, char bef, char bef2)
+{
+	if ((now == tmp && tmp == S_QUOTE))
+		return (1);
+	if (tmp == D_QUOTE && now == tmp)
+	{
+		if ((bef != SLASH) || (bef2 == SLASH && bef == SLASH))
+			return (1);
+	}
+	return (0);
+}
+
+static int		is_second_condition(char now, char bef, int i)
+{
+	if (now == S_QUOTE)
+		return (1);
+	if ((i > 0 && (now == D_QUOTE && bef != SLASH))
+	|| (i == 0 && now == D_QUOTE))
+		return (1);
+	return (0);
+}
+
 int		multiple_semicolon(char *str)
 {
-	int i;
+	int		i;
+	int		in_quote;
+	int		flag;
+	char	tmp;
 
+	flag = 0;
+	in_quote = -1;
 	i = 0;
 	if (str[0] == ';')
 		return (0);
 	while (str[i + 1])
 	{
-		if (str[i] == ';' && str[i + 1] == ';')
-			return (0);
-		if (str[i] == ';')
-		{
+		if (str[i] == SLASH && (str[i + 1] == SLASH
+		|| str[i + 1] == D_QUOTE || (in_quote == -1 && str[i + 1] == S_QUOTE)))
 			i++;
+		else if (in_quote == 1 && ((i > 1 && is_first_condition(tmp, str[i],
+		str[i - 1], str[i - 2])) || (i > 0 && is_first_condition(tmp, str[i],
+		str[i - 1], SLASH))))
+			in_quote = -in_quote;
+		else if (in_quote == -1)
+		{
+			if (str[i] == ';' && flag == 0)
+				flag = 1;
+			else if (str[i] == ';' && flag == 1)
+				return (0);
 			while (str[i] && str[i] == ' ')
 				i++;
-			if (str[i] == ';')
-				return (0);
 		}
 		i++;
 	}
