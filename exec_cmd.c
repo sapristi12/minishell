81e0e4ce835acb59b704f68d68728309b8e9a073
Erwan Lajouannique <er.lajouannique@gmail.com>
2021-03-09 13:11:40 +0100

    cd home

--- exec_cmd.c
+++ exec_cmd.c
@@ -69,6 +69,7 @@ static int		execution_bin(t_cmd *cmd, t_list **envs, int i)
 			ft_lstclear(envs, free);
 			execve(cmd->pipe.all[i][0], cmd->pipe.all[i], envp);
 			free_char_double_array(envp);
+			free_package(cmd);
 		}
 		return (-1);
 	}
