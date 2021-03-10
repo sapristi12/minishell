102a45b6ae391ff4923dfc732192629d73697784
Erwan Lajouannique <er.lajouannique@gmail.com>
2021-03-10 11:35:01 +0100

    echo changed

--- ft_echo.c
+++ ft_echo.c
@@ -21,7 +21,7 @@ int		ft_echo(t_cmd *cmd, int index)
 	size = 1;
 	flag = 0;
 	i = 1;
-	while (cmd->cmds[size] && !is_symbol(cmd->cmds[size]) || (is_symbol(cmd->cmds[size]) && cmd->tab[index][size] == 0))
+	while (cmd->cmds[size] && (!is_symbol(cmd->cmds[size]) || (is_symbol(cmd->cmds[size]) && cmd->tab[index][size] == 0)))
 		size++;
 	while (cmd->cmds[i] && !ft_strcmp(cmd->cmds[i], "-n"))
 	{
