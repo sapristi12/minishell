7c06a6de3afbf9ba662e0820c387495a094a2eb3
Erwan Lajouannique <er.lajouannique@gmail.com>
2021-03-11 16:05:09 +0100

    ALL GOOD :pig:

--- check_first_command.c
+++ check_first_command.c
@@ -6,7 +6,7 @@
 /*   By: erlajoua <erlajoua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 15:50:10 by erlajoua          #+#    #+#             */
-/*   Updated: 2021/03/11 15:58:35 by erlajoua         ###   ########.fr       */
+/*   Updated: 2021/03/11 16:04:32 by erlajoua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ void	is_command_nf(t_cmd *cmd, t_list **envs)
 		return ;
 	if (path && !is_builtin(command))
 		return ;
-	else if (!is_builtin(command) && !path && stat(command, &myst))
+	else if (!is_builtin(command) && !path)
 	{
-		if (g_sig == 0)
+		if (g_sig == 0 && stat(command, &myst))
 			g_sig = 127;
 		if (command && is_dir(command, 0))
 			g_sig = 126;
